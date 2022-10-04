58ac99219436476dd2277bb9fdf5bbda0f6bfec3
Alp Senay <alps2789@nycstudents.net>
2022-10-04 09:31:17 -0400

    a

---
+++ src/Point.java
@@ -0,0 +1,55 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(int xy){
+        x = xy;
+        y = xy;
+    }
+
+    public Point(){
+        x = 0;
+        y = 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int newX){
+        x = newX;
+    }
+
+    public void setY(int newY){
+        y = newY;
+    }
+
+    public void coordinate(){
+        System.out.println("(" + x + ", " + y);
+    }
+
+    public String quadrant(){
+        if (x > 0 && y > 0){
+
+        }
+        if (x < 0 && y > 0){
+
+        }
+        if (x > 0 && y < 0){
+
+        }
+        if (x < 0 && y > 0){
+
+        }
+    }
+
+}
